be9b8ec3160903d2e59fc9b949abb5262a772e0c
Cosmo Colman <cosmo.colman@kcl.ac.uk>
2022-10-06 18:24:48 +0100

    Exercise 2: Add a basic things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Thing(Model):
+    name = models.TextField()
+    description = models.CharField(max_length=255)
+    quantity = models.IntegerField()
